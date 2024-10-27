0919b4562d9cf8d5cc832442f0ccd65f26bff67d
Odissey2806 <aleksand2806@ya.ru>
2024-10-27 22:33:20 +0700

    Домашняя работа 3

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
 
-        // Условный оператор Урок2
+        // Условный оператор Урок 2
 
         // Задача № 1
 
